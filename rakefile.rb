fb02617bef69fd2dce2c9800bcc33a2c155b1443
Andrzej Śliwa <andrzej.sliwa@i-tool.eu>
2010-02-04 13:31:55 +0100

    some customization

--- rakefile.rb
+++ rakefile.rb
@@ -17,11 +17,13 @@ title: "Consileon PL - DevBlog"
 description: ""
 ---
 
-<div class="related">
-  <div id="rest">
-    <h1>Kategoria \"#{category}\":</h1>
-    <p>{% for post in site.categories.#{category} %}<a href="{{ post.url }}">{{ post.title }}</a>{% unless forloop.last %} &middot; {% endunless %}{% endfor %}</p>
-  </div>
+<div id="entries">
+  <h1>Kategoria \"#{category}\":</h1>
+  <ul> 
+    {% for post in site.categories.#{category} %}
+    <li><a href="{{ post.url }}">{{ post.title }}</a></li>
+    {% endfor %}
+  </ul>
 </div>    
 HTML
     File.open("tags/#{category}.html", 'w+') do |file|
